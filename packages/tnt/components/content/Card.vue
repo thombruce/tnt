5879d2d35d10cf45cdc6958e645d975bc3154bd5
Thom Bruce <thom@thombruce.com>
2023-08-16 18:13:52 +0100

    add link style to general links in cards

--- packages/tnt/components/content/Card.vue
+++ packages/tnt/components/content/Card.vue
@@ -4,15 +4,19 @@ defineProps(['image'])
 
 <template>
 <div class="card flex-1">
-    <figure v-if="image"><img :src="image" /></figure>
-    <div class="card-body not-prose">
-        <ContentSlot :use="$slots.default" />
-    </div>
+  <figure v-if="image"><img :src="image" /></figure>
+  <div class="card-body not-prose">
+    <ContentSlot :use="$slots.default" />
+  </div>
 </div>
 </template>
 
 <style lang="postcss">
 .card-body h1:first-child, .card-body h2:first-child, .card-body h3:first-child {
   @apply card-title;
+}
+
+.card-body p a {
+  @apply link;
 }
 </style>
