ffcffaf838bbbd017f21eb18b19c96fe30ae7d1b
Thom Bruce <thom@thombruce.com>
2023-08-16 23:25:09 +0100

    add convenience config for Umami

--- packages/tnt/app.vue
+++ packages/tnt/app.vue
@@ -1,3 +1,16 @@
+<script setup>
+const { name, umami } = useAppConfig()
+
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - ${name}` : name
+  },
+  script: umami && process.env.NODE_ENV === 'production' ?
+    [{ async: true, src: `https://${umami.domain}/script.js`, 'data-website-id': `${umami.id}` }] :
+    undefined
+})
+</script>
+
 <template>
   <div>
     <AppNavbar />
