e0e399bdf6dbac25f271792772bb116981995f7a
Thom Bruce <thom@thombruce.com>
2023-08-25 12:59:03 +0100

    move Font Awesome plugin script to component

--- packages/tnt/components/content/Fa.vue
+++ packages/tnt/components/content/Fa.vue
@@ -1,5 +1,20 @@
 <script setup>
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
 defineProps(['icon'])
+
+// This is important, we are going to let Nuxt worry about the CSS
+config.autoAddCss = false
+
+library.add(far, fas, fab)
+
+defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {})
+})
 </script>
 
 <template>
