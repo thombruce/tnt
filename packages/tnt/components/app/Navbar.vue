aaca774f4fc0299b1505dc9223d1e59e38e4b1fd
Thom Bruce <thom@thombruce.com>
2023-09-04 21:54:01 +0100

    add base color to navbar and make sticky

--- packages/tnt/components/app/Navbar.vue
+++ packages/tnt/components/app/Navbar.vue
@@ -3,7 +3,7 @@ const appConfig = useAppConfig()
 </script>
 
 <template>
-  <div class="navbar bg-base-100">
+  <div class="navbar bg-base-300 sticky top-0 z-50">
     <div class="flex-1">
       <NuxtLink to="/" class="btn btn-ghost normal-case text-xl">{{ appConfig.name }}</NuxtLink>
     </div>
