c8eb2160ff2045e56c8ce9d4c41baa606cec295f
Thom Bruce <thom@thombruce.com>
2024-04-25 04:15:05 +0100

    remove fountain transformer

--- packages/content/layouts/fountain.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug">
-ContentDoc(#default="{ doc }")
-  .prose.mx-auto
-    h1 {{ doc.body.title }}
-    Breadcrumbs
-  .screenplay
-    .title-page(v-html="doc.body.html.title_page")
-    .script(v-html="doc.body.html.script")
-</template>
--- packages/content/nuxt.config.ts
+++ packages/content/nuxt.config.ts
@@ -2,28 +2,13 @@
 
 import { createResolver, defineNuxtModule } from '@nuxt/kit'
 
-const FountainJS = defineNuxtModule({
-  async setup(_options, nuxt) {
-    const { resolve } = createResolver(import.meta.url)
-    nuxt.options.nitro.externals = nuxt.options.nitro.externals || {}
-    nuxt.options.nitro.externals.inline = nuxt.options.nitro.externals.inline || []
-    nuxt.options.nitro.externals.inline.push(resolve('./fountain'))
-    // @ts-ignore
-    nuxt.hook('content:context', (contentContext) => {
-      contentContext.transformers.push(resolve('./fountain/transformer.ts'))
-    })
-  }
-})
-
 export default defineNuxtConfig({
   extends: [
     '@thombruce/tnt'
   ],
   modules: [
-    FountainJS,
     '@nuxt/content',
   ],
-  css: ['@thombruce/fountainjs/src/fountain.css'],
   content: {
     documentDriven: true,
     respectPathCase: true,
