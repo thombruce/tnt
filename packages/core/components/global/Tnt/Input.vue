97f4d8d3bd6e4a12e5c6b7280ad9481f691fe4d3
Thom Bruce <thom@thombruce.com>
2024-07-11 19:10:17 +0100

    implement form input styles

--- packages/core/components/global/Tnt/Input.vue
+++ packages/core/components/global/Tnt/Input.vue
@@ -27,24 +27,36 @@ defineEmits([
 ])
 </script>
 
-<template>
-  <div class="form-control">
-    <label v-if="label || labelAlt" :for="id" class="label">
-      <span v-if="label" class="label-text" v-html="label" />
-      <span v-if="labelAlt" class="label-text-alt" v-html="labelAlt" />
-    </label>
-    <input
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-      :id="id"
-      :name="name"
-      :type="type"
-      :placeholder="placeholder"
-      class="input input-bordered"
-    />
-    <label v-if="hint || hintAlt" :for="id" class="label">
-      <span v-if="hint" class="label-text-alt" v-html="hint" />
-      <span v-if="hintAlt" class="label-text-alt" v-html="hintAlt" />
-    </label>
-  </div>
+<template lang="pug">
+div
+  label(v-if="label || labelAlt" :for="id")
+    span(v-if="label" v-html="label")
+    span(v-if="labelAlt" v-html="labelAlt")
+  input(
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    :id="id"
+    :name="name"
+    :type="type"
+    :placeholder="placeholder"
+  )
+  label(v-if="hint || hintAlt" :for="id")
+    span(v-if="hint" v-html="hint")
+    span(v-if="hintAlt" v-html="hintAlt")
 </template>
+
+<style lang="postcss" scoped>
+input {
+  @apply
+    py-2
+    px-3
+    rounded
+    border
+    bg-white
+    text-gray-700
+    border-gray-200
+    dark:bg-gray-900
+    dark:text-gray-300
+    dark:border-gray-700;
+}
+</style>
