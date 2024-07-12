bf40c06b5b4404a4a352eaf6126cc27c3bad8130
Thom Bruce <thom@thombruce.com>
2024-07-12 05:49:54 +0100

    rework range type input styles

--- packages/core/components/global/Tnt/Range.vue
+++ packages/core/components/global/Tnt/Range.vue
@@ -26,23 +26,38 @@ defineEmits([
 ])
 </script>
 
-<template>
-  <div>
-    <label v-if="label" :for="id" class="label">
-      <span v-if="label" class="label-text" v-html="label" />
-    </label>
-    <input
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-      :id="id"
-      :name="name"
-      type="range"
-      :min="min"
-      :max="max"
-      class="range"
-    />
-    <label v-if="hint" :for="id" class="label">
-      <span v-if="hint" class="label-text-alt" v-html="hint" />
-    </label>
-  </div>
+<template lang="pug">
+div
+  label(v-if="label" :for="id")
+    span.font-bold(v-if="label" v-html="label")
+  input(
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    :id="id"
+    :name="name"
+    type="range"
+    :min="min"
+    :max="max"
+  )
+  label(v-if="hint" :for="id")
+    span.text-xs.text-gray-500(v-if="hint" v-html="hint")
 </template>
+
+<style lang="postcss" scoped>
+input {
+  @apply
+    block
+    w-full
+    py-2
+    accent-blue-600
+    hover:accent-blue-700
+    active:accent-blue-700;
+}
+
+/*
+Pseudo-states for range input:
+  hover:
+  focus:
+  active:
+*/
+</style>
