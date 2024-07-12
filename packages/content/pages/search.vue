bb358933616fcb1079caeaf809699d2d85b3518e
Thom Bruce <thom@thombruce.com>
2024-07-12 22:10:24 +0100

    add basic search page

---
+++ packages/content/pages/search.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const query = ref('')
+const results = ref([])
+
+const search = async () => {
+  const res = await searchContent(query.value, {})
+  results.value = res.value
+}
+</script>
+
+<template lang="pug">
+div
+  TntInput(
+    placeholder="Search..."
+    v-model="query"
+    @input="search()"
+  )
+
+  pre {{ results }}
+</template>
