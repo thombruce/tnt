c5d67cbca4676129f7b06097f88d29f4f4590c9a
Thom Bruce <thom@thombruce.com>
2024-07-13 20:21:05 +0100

    add reusable Search component

--- packages/content/pages/search.vue
+++ packages/content/pages/search.vue
@@ -10,13 +10,20 @@ const search = async () => {
 
 <template lang="pug">
 div
-  TntInput(
+  TntSearch(
     placeholder="Search..."
     v-model="query"
-    @input="search()"
+    :method="search"
   )
 
+  //- TODO: It is probably better to use the ID to fetch the content itself for display (which will include nav title if present).
+  //-       Drawback? Won't link to subheadings. But... you can always use the id for that.
+  //-       Lot of design considerations here.
   ul
-    li(v-for="article in results" :key="article.id")
-      NuxtLink(:to="article.id") {{ article.navigation?.title || article.title }}
+    li.mb-5(v-for="article in results" :key="article.id")
+      NuxtLink(:to="article.id")
+        strong {{ article.navigation?.title || article.title }}
+      p {{ article.content }}
+
+  //- pre {{ results }}
 </template>
