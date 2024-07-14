18fc8f1c75e911379c73a754fee237d7843cab7d
Thom Bruce <thom@thombruce.com>
2024-07-14 19:39:01 +0100

    add redirect layout

---
+++ packages/content/layouts/redirect.vue
@@ -0,0 +1,18 @@
+<script setup>
+const { page } = useContent()
+
+useHead({
+  title: `Redirecting to ${page.value.title}...`,
+  meta: [{ "http-equiv": "refresh", content: `0; URL=${page.value.link}` }],
+  link: [{ rel: "canonical", href: page.value.link }]
+})
+</script>
+
+<template lang="pug">
+.prose
+  h1 Redirecting to {{ page.title }}...
+  p
+    | If you are not redirected automatically, click here:
+    |
+    NuxtLink(:to="page.link") {{ page.link }}
+</template>
