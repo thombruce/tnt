40931dae2412f2f33b1b98ab8a56efa0b3e02a35
Thom Bruce <thom@thombruce.com>
2024-07-14 01:37:05 +0100

    add nav link to search page

--- packages/content/components/app/Navigation.vue
+++ packages/content/components/app/Navigation.vue
@@ -1,4 +1,7 @@
 <template lang="pug">
 ContentNavigation(v-slot="{ navigation }")
   NuxtLink(v-for="link of navigation" :to="link._path" :key="link._path") {{ link.title }}
+NuxtLink(to="/search")
+  Icon(name="fa:search")
+  span.sr-only Search
 </template>
