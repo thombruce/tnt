906aad4511a6986ee842f597769ed3fb513e461d
Thom Bruce <thom@thombruce.com>
2024-07-14 05:23:21 +0100

    improve Footer scoping

---
+++ packages/content/components/app/FooterHidden.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
+const query: QueryBuilderParams = { path: '/', where: [{ navigation: { $eq: false }, _dir: '' }] }
+</script>
+
+<template lang="pug">
+.hidden
+  ContentList(:query="query")
+    template(#default="{ list }")
+      ul
+        li(v-for="article in list" :key="article._path")
+          NuxtLink(:to="article._path") {{ article.title }}
+    template(#not-found)
+</template>
--- packages/core/components/app/Footer.vue
+++ packages/core/components/app/Footer.vue
@@ -1,8 +1,9 @@
-<script setup lang="ts">
+<script setup>
 const { copyright } = useAppConfig()
 </script>
 
 <template lang="pug">
-footer.mt-4
+footer.mt-4.text-center
   small {{ copyright }}
+  AppFooterHidden
 </template>
