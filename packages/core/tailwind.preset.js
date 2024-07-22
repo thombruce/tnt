be2a2789bd0580558fbc1a74618ec55f0908c04e
Thom Bruce <thom@thombruce.com>
2024-07-22 14:49:48 +0100

    extend Tailwind color palette

--- packages/core/tailwind.preset.js
+++ packages/core/tailwind.preset.js
@@ -2,6 +2,64 @@
 
 const colors = require('tailwindcss/colors')
 
+const palette = {
+  neutral: {...colors.zinc, ...{
+    DEFAULT: colors.zinc['500'],
+  }},
+
+  primary: {...colors.sky, ...{
+    DEFAULT: colors.sky['500'],
+    hover: colors.sky['600'],
+    light: {
+      DEFAULT: colors.sky['500'],
+      hover: colors.sky['600'],
+    },
+    dark: {
+      DEFAULT: colors.sky['600'],
+      hover: colors.sky['500'],
+    },
+  }},
+
+  danger: {...colors.red, ...{
+    DEFAULT: colors.red['500'],
+    hover: colors.red['600'],
+    light: {
+      DEFAULT: colors.red['500'],
+      hover: colors.red['600'],
+    },
+    dark: {
+      DEFAULT: colors.red['600'],
+      hover: colors.red['500'],
+    },
+  }},
+
+  warning: {...colors.amber, ...{
+    DEFAULT: colors.amber['500'],
+    hover: colors.amber['600'],
+    light: {
+      DEFAULT: colors.amber['500'],
+      hover: colors.amber['600'],
+    },
+    dark: {
+      DEFAULT: colors.amber['600'],
+      hover: colors.amber['500'],
+    },
+  }},
+
+  success: {...colors.green, ...{
+    DEFAULT: colors.green['500'],
+    hover: colors.green['600'],
+    light: {
+      DEFAULT: colors.green['500'],
+      hover: colors.green['600'],
+    },
+    dark: {
+      DEFAULT: colors.green['600'],
+      hover: colors.green['500'],
+    },
+  }},
+}
+
 export default {
   plugins: [
     require('@tailwindcss/typography'),
@@ -11,30 +69,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        base: {...colors.zinc, ...{
-          DEFAULT: colors.zinc['50'],
-          light: colors.zinc['50'],
-          dark: colors.zinc['950'],
-        }},
-
-        content: {...colors.zinc, ...{
-          DEFAULT: colors.zinc['900'],
-          light: colors.zinc['900'],
-          dark: colors.zinc['100'],
-        }},
-
-        primary: {...colors.sky, ...{
-          DEFAULT: colors.sky['500'],
-          hover: colors.sky['600'],
-          light: {
-            DEFAULT: colors.sky['500'],
-            hover: colors.sky['600'],
-          },
-          dark: {
-            DEFAULT: colors.sky['600'],
-            hover: colors.sky['500'],
-          },
-        }},
+        ...palette,
+        ...{
+          base: { ...palette.neutral, ...{
+            DEFAULT: palette.neutral['50'],
+            light: palette.neutral['50'],
+            dark: palette.neutral['950'],
+          }},
+          content: {...palette.neutral, ...{
+            DEFAULT: palette.neutral['900'],
+            light: palette.neutral['900'],
+            dark: palette.neutral['100'],
+          }},
+          error: palette.danger,
+        }
       },
     },
   },
