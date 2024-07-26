b128f96275647991c6de0b3aa32c46a64a773dee
Thom Bruce <thom@thombruce.com>
2024-07-26 15:47:56 +0100

    convert file select component to VeeValidate

--- packages/core/components/global/Tnt/Select.vue
+++ packages/core/components/global/Tnt/Select.vue
@@ -15,6 +15,16 @@ const props = defineProps({
   hint: {},
   placeholder: {},
   options: {}, // array of strings or { value, text } objects
+  fullErrors: {
+    type: Boolean
+  },
+  rules: {
+    type: Object,
+  }
+})
+
+const computedRules = computed(() => {
+  return useValidations(props.rules?.format || props.type, props.rules, props.label)
 })
 
 defineEmits([
@@ -23,17 +33,36 @@ defineEmits([
 </script>
 
 <template lang="pug">
-div
-  label(v-if="label" :for="id")
-    span.font-bold(v-if="label" v-html="label")
-  select(
-    :value="modelValue"
+div(:class="fullErrors ? 'full-errors' : undefined")
+  //- @input changed to @change
+  VeeField(
+    v-bind="modelValue"
     @change="$emit('update:modelValue', $event.target.value)"
-    :id="id"
     :name="name"
+    :rules="computedRules"
+    v-slot="{ field, value, errors }"
   )
-    option(disabled selected) {{ placeholder }}
-    option(v-for="option in options" :value="option.value || option") {{ option.text || option }}
-  label(v-if="hint" :for="id")
-    span.text-xs.text-gray-500(v-if="hint" v-html="hint")
+    label(v-if="label" :for="id")
+      span.font-bold(v-if="label" v-html="label")
+
+    select(
+      v-bind="field"
+      :id="id"
+      :class="errors[0] ? 'error' : ''"
+    )
+      option(disabled selected) {{ placeholder }}
+      option(
+        v-for="option in options"
+        :key="option"
+        :value="option.value || option.text || option"
+        :selected="value && value.includes(option.value || option.text || option)"
+      ) {{ option.text || option.value || option }}
+
+    .errors.full
+      ul(class="text-error marker:text-error-500 dark:text-error-dark marker:dark:text-error-900")
+        li(v-for="error in errors") {{ error }}
+    .errors.single(class="text-error dark:text-error-dark") {{ errors[0] }}
+
+    label(v-if="hint" :for="id")
+      span.text-xs.text-gray-500(v-if="hint" v-html="hint")
 </template>
