327a33f609ae7a27ace56e92cc4d1278a92d9997
Thom Bruce <thom@thombruce.com>
2024-07-28 05:19:42 +0100

    allow FromSchema items to have no body

--- packages/core/components/global/FromSchema.vue
+++ packages/core/components/global/FromSchema.vue
@@ -14,7 +14,7 @@ component(
   v-bind="typeof component[type] === 'object' ? _omit(component[type], ['body']) : undefined"
 )
   template(v-if="typeof component[type] === 'string'") {{ component[type] }}
-  template(v-else-if="typeof component[type].body === 'string'") {{ component[type].body }}
-  template(v-else-if="component[type].body" v-for="component in component[type].body")
+  template(v-else-if="typeof component[type]?.body === 'string'") {{ component[type].body }}
+  template(v-else-if="component[type]?.body" v-for="component in component[type].body")
     FromSchema(:component="component")
 </template>
