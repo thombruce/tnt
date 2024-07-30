98a3a4f1130f25a2e1d93bb93d32ff871de63ff0
Thom Bruce <thom@thombruce.com>
2024-07-30 14:25:45 +0100

    refactor Byline for clarity

--- packages/content/components/content/Byline.vue
+++ packages/content/components/content/Byline.vue
@@ -9,26 +9,22 @@ const route = useRoute()
 
 let path = props.path || route.path
 
-const { data } =
+const { data: rawData } =
   props.authors
   ? { data: { authors: props.authors } }
   : props.page
-  ? { data: props.page }
+  ? { data: { authors: props.page.authors } }
   : await useAsyncData(`tnt-byline-${path}`, () => queryContent(path).only('authors').findOne())
+
+const data = [rawData.authors || rawData.value?.authors].flat()
 </script>
 
 <template lang="pug">
-div(v-if="data.authors")
+div(v-if="data")
   span.italic
     | {{ `Written by ` }}
-
-    template(
-      v-if="!(typeof data.authors === 'string')"
-      v-for="(author, index) in data.authors"
-    )
+    template(v-for="(author, index) in data")
       NuxtLink(:to="`/~authors/${_kebabCase(author)}`") {{ author }}
-      template(v-if="index < data.authors.length - 2") {{ `, ` }}
-      template(v-if="index == data.authors.length - 2") {{ ` & ` }}
-
-    NuxtLink(v-else :to="`/~authors/${_kebabCase(data.authors)}`") {{ data.authors }}
+      template(v-if="index < data.length - 2") {{ `, ` }}
+      template(v-if="index == data.length - 2") {{ ` & ` }}
 </template>
