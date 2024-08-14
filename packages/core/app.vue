c6dac3c60e765468430774bc782c8e5644b33bd0
Thom Bruce <thom@thombruce.com>
2024-08-14 14:32:29 +0100

    remove note

--- packages/core/app.vue
+++ packages/core/app.vue
@@ -6,13 +6,6 @@ const colorMode = useColorMode()
 const config = await useTntApi().loadConfig()
 if (config.colorMode) colorMode.preference = config.colorMode
 
-// NOTE: Alternative pattern; data is returned via IPC send
-// if (isElectron()) {
-//   window.api.on('return-config', (_, data) => {
-//     colorMode.preference = data.colorMode
-//   })
-// }
-
 useHead({
   titleTemplate: (titleChunk) => {
     return titleChunk ? `${titleChunk} - ${name}` : name
