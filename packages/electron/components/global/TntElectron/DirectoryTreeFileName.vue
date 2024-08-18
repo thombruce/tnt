d7c8dd97be76e36a3253e86336b33c5122305370
Thom Bruce <thom@thombruce.com>
2024-08-19 00:46:05 +0100

    reflect changed name values in directory tree display

--- packages/electron/components/global/TntElectron/DirectoryTreeFileName.vue
+++ packages/electron/components/global/TntElectron/DirectoryTreeFileName.vue
@@ -20,25 +20,34 @@ const { root } = storeToRefs(store)
 const { renameFile } = store
 
 function rename() {
+  // TODO: This breaks for subsequent edits - name prop is not reactive
+  // TODO: Nested items also cannot be updated if the directory name has also already been changed
+  //       Again, the props aren't reactive!
   const regex = new RegExp(`${props.name}$`)
   const newPath = props.path.replace(regex, value.value)
   renameFile(props.path, newPath)
   editing.value = false
 }
+
+function cancel() {
+  editing.value = false
+  // TODO: This breaks if already edited - name prop is not reactive
+  value.value = props.name
+}
 </script>
 
 <template lang="pug">
-TntForm.inline(v-if="editing" @submit="rename()" @keydown.esc="editing = false")
+TntForm.inline(v-if="editing" @submit="rename()" @keydown.esc="cancel()")
   TntInput.inline-block(v-model="value")
   TntSubmit.btn-none.ml-2
     Icon(name="fa6-solid:floppy-disk")
 template(v-else)
   NuxtLink(v-if="link" :to="path.replace(`${root}/`, '')")
-    span {{ name }}
+    span {{ value }}
     TntButton.btn-none(@click.prevent="editing = true")
       Icon.ml-2(name="fa6-solid:pencil")
   template(v-else)
-    span {{ name }}
+    span {{ value }}
     TntButton.btn-none(@click.prevent="editing = true")
       Icon.ml-2(name="fa6-solid:pencil")
 </template>
