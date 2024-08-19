c0e7c06128d07ee943e81202dfb02eb832f0ba6d
Thom Bruce <thom@thombruce.com>
2024-08-19 01:05:37 +0100

    enable subsequent edits of same file in tree

--- packages/electron/components/global/TntElectron/DirectoryTreeFileName.vue
+++ packages/electron/components/global/TntElectron/DirectoryTreeFileName.vue
@@ -2,13 +2,14 @@
 import { useDirectoryStore } from '@/stores/directory'
 import { storeToRefs } from 'pinia'
 
+const path = defineModel('path')
+const name = defineModel('name')
+
 const props = defineProps([
-  'path',
-  'name',
   'link',
 ])
 
-const value = ref(props.name)
+const initialName = ref(name.value)
 
 const editing = ref(false)
 
@@ -20,34 +21,32 @@ const { root } = storeToRefs(store)
 const { renameFile } = store
 
 function rename() {
-  // TODO: This breaks for subsequent edits - name prop is not reactive
-  // TODO: Nested items also cannot be updated if the directory name has also already been changed
-  //       Again, the props aren't reactive!
-  const regex = new RegExp(`${props.name}$`)
-  const newPath = props.path.replace(regex, value.value)
-  renameFile(props.path, newPath)
+  const regex = new RegExp(`${initialName.value}$`)
+  const newPath = path.value.replace(regex, name.value)
+  renameFile(path.value, newPath)
+  path.value = newPath
+  initialName.value = name.value
   editing.value = false
 }
 
 function cancel() {
+  name.value = initialName.value
   editing.value = false
-  // TODO: This breaks if already edited - name prop is not reactive
-  value.value = props.name
 }
 </script>
 
 <template lang="pug">
 TntForm.inline(v-if="editing" @submit="rename()" @keydown.esc="cancel()")
-  TntInput.inline-block(v-model="value")
+  TntInput.inline-block(v-model="name")
   TntSubmit.btn-none.ml-2
     Icon(name="fa6-solid:floppy-disk")
 template(v-else)
   NuxtLink(v-if="link" :to="path.replace(`${root}/`, '')")
-    span {{ value }}
+    span {{ name }}
     TntButton.btn-none(@click.prevent="editing = true")
       Icon.ml-2(name="fa6-solid:pencil")
   template(v-else)
-    span {{ value }}
+    span {{ name }}
     TntButton.btn-none(@click.prevent="editing = true")
       Icon.ml-2(name="fa6-solid:pencil")
 </template>
