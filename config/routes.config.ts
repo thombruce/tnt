82ebb251bd988a08f3bfcee5a334f4c09660046f
Thom Bruce <thom@thombruce.com>
2025-03-31 17:55:31 +0100

    add demo components section to docs

--- config/routes.config.ts
+++ config/routes.config.ts
@@ -1,3 +1,5 @@
 export default {
+  '/docs': { redirect: '/docs/getting-started/installation', prerender: false },
   '/docs/getting-started': { redirect: '/docs/getting-started/installation', prerender: false },
+  '/docs/components': { redirect: '/docs/components/installation', prerender: false },
 }
