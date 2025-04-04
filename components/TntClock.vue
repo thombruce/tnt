b960ec3ec4347357e9208f900ad94bd560221bdc
Thom Bruce <thom@thombruce.com>
2025-04-04 02:29:11 +0100

    update clock to use internationalized date library

--- components/TntClock.vue
+++ components/TntClock.vue
@@ -3,15 +3,30 @@
 //       It may not when rendered statically...?
 //       Investigate and... probably move to a
 //       components/global/ dir if that's the case.
-const { type = 'digital' } = defineProps<{
-  type?: 'digital' | 'analog'
+import { fromDate, toTime, toTimeZone, toLocalTimeZone } from '@internationalized/date'
+
+const { type = 'digital', timezone } = defineProps<{
+  type?: 'digital' | 'analog', // timer | countdown
+  timezone?: string,
+  // 24hr vs 12hr
 }>()
 
-const time = ref((new Date))
+const getTimeNow = () => fromDate(new Date(), 'UTC')
 
-useIntervalFn(() => time.value = new Date)
+const time = ref(getTimeNow())
+
+// TODO: It is safe to move this interval to a composable
+//       since conversion is now handled separately below.
+useIntervalFn(() => time.value = getTimeNow(), 500)
+
+const timeInZone = computed(() => {
+  const t = timezone
+    ? toTimeZone(time.value, timezone)
+    : toLocalTimeZone(time.value)
+  return toTime(t)
+})
 </script>
 
 <template lang="pug">
-p {{ time.toLocaleTimeString() }}
+span {{ timeInZone.toString().split('.')[0] }}
 </template>
--- content/1.docs/2.components/clock.md
+++ content/1.docs/2.components/clock.md
@@ -1,7 +1,17 @@
 # Clock
 
+## UTC
+
 ```js
 <TntClock />
 ```
 
 :tnt-clock
+
+## With Timezone
+
+```js
+<TntClock timezone="America/Los_Angeles" />
+```
+
+:tnt-clock{timezone="America/Los_Angeles"}
