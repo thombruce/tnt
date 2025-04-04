189b4f0b6a85c253979a19fb4207bb4b50c20115
Thom Bruce <thom@thombruce.com>
2025-04-04 20:29:39 +0100

    improve readibility/extensibility of tntContentCollections util

--- utils/tntContentCollections.ts
+++ utils/tntContentCollections.ts
@@ -1,4 +1,8 @@
 import type { PageCollections } from "@nuxt/content"
 
 // TODO: We'd rather import the keys from collections directly if that's at all possible.
-export const tntContentCollections: (keyof PageCollections)[] = ['pages', 'blog', 'docs']
+export const tntContentCollections: (keyof PageCollections)[] = [
+  'pages',
+  'blog',
+  'docs',
+]
