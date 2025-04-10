ef23d22afd5236207cc19ff6e808307f65b7fe02
Thom Bruce <thom@thombruce.com>
2025-04-10 14:13:32 +0100

    add basic types to config files

--- config/routes.config.ts
+++ config/routes.config.ts
@@ -1,4 +1,9 @@
 export default {
   '/docs': { redirect: '/docs/getting-started/installation', prerender: false },
   '/docs/getting-started': { redirect: '/docs/getting-started/installation', prerender: false },
+} as RoutesConfig
+
+/* TYPES */
+type RoutesConfig = {
+  [k: string]: object
 }
--- config/site.config.ts
+++ config/site.config.ts
@@ -1,23 +1,20 @@
 import type { NavigationMenuItem } from "@nuxt/ui"
 
-const name: string | null = null
-
-const description: string | null = null
-
-const url: string | null = null
-
-const copyright: string | null = null
-
-// TODO: defaultLocale is currently unused. Determine whether we need this or if i18n should be handled differently.
-const defaultLocale: string = 'en'
-
-const nav: NavigationMenuItem[] | boolean = true
-
 export default {
-  name,
-  description,
-  url,
-  copyright,
-  defaultLocale,
-  nav,
+  name: null,
+  description: null,
+  url: null,
+  copyright: null,
+  defaultLocale: 'en',
+  nav: true,
+} as SiteConfig
+
+/* TYPES */
+type SiteConfig = {
+  name: string | null,
+  description: string | null,
+  url: string | null,
+  copyright: string | null,
+  defaultLocale: string,
+  nav: NavigationMenuItem[] | boolean,
 }
