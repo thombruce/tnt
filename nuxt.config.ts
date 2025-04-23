9d0a786463af37e787e59203a8e9a567ad5a2528
Thom Bruce <thom@thombruce.com>
2025-04-23 19:38:31 +0100

    correct module ordering / revert extension of core from content package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   extends: [
+    './packages/core',
     './packages/content',
   ],
   modules: [
--- packages/content/nuxt.config.ts
+++ packages/content/nuxt.config.ts
@@ -7,9 +7,6 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  extends: [
-    '../core',
-  ],
   modules: [
     '@nuxt/content',
   ],
