10fe87ad858575ffb963e1a952b2e89cb7dc7edc
Thom Bruce <thom@thombruce.com>
2025-04-27 09:50:09 +0100

    make footer 'sticky'

--- packages/core/layouts/base.vue
+++ packages/core/layouts/base.vue
@@ -3,13 +3,13 @@ const runtimeConfig = useRuntimeConfig()
 </script>
 
 <template lang="pug">
-div
+div(class="flex flex-col h-screen justify-between")
   //- TODO: Maybe prefer to gracefully enhance content components
   //-       based on runtimeConfig, rather than duplicating or
   //-       overwriting them like this.
   TntContentHeader(v-if="runtimeConfig?.public?.content")/
   TntHeader(v-else)/
-  UContainer
+  UContainer(class="mb-auto")
     slot/
   TntFooter/
 </template>
