a35d75e79e402228af8eb204e59541d33a803fc0
Thom Bruce <thom@thombruce.com>
2025-05-04 16:13:44 +0100

    add drawer navigation menu for mobile view

--- packages/content/components/TntContentHeader.vue
+++ packages/content/components/TntContentHeader.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { UDrawer } from '#components';
+
 const { name, nav: navConfig } = useSiteConfig()
 
 const { data: navItems } = await useAsyncData(`tntNav-for-content`, () => {
@@ -19,14 +21,27 @@ div(class="w-full \
           items-center \
           justify-between"
   )
-    UButton(
-      :label="name"
-      to="/"
-      color="neutral"
-      variant="ghost"
-      class="my-2 px-2.5 py-1.5 font-extrabold"
-    )/
-    UNavigationMenu(:items="navItems || undefined" class="w-full justify-center z-50")/
+    .flex
+      UDrawer(direction="left" class="md:hidden")
+        UButton(
+          icon="i-lucide:menu"
+          color="neutral"
+          variant="ghost"
+          class="my-2 px-2.5 py-1.5"
+        )
+        template(#content)
+          UNavigationMenu(:items="navItems || undefined" orientation="vertical")/
+
+      UButton(
+        :label="name"
+        to="/"
+        color="neutral"
+        variant="ghost"
+        class="my-2 px-2.5 py-1.5 font-extrabold"
+      )/
+
+    UNavigationMenu(:items="navItems || undefined" class="hidden md:flex w-full justify-center z-50")/
+
     .flex
       TntSearch/
       TntDarkMode/
