ec87b1cb6143b03326b24b4961e24902f9b0f8ee
Thom Bruce <thom@thombruce.com>
2025-05-04 16:18:43 +0100

    hide side nav and toc on smaller screens

--- packages/content/layouts/article.vue
+++ packages/content/layouts/article.vue
@@ -7,15 +7,17 @@ const { collection = 'pages' }: { collection?: keyof PageCollections | undefined
 <template lang="pug">
 NuxtLayout(name="base")
   .grid.grid-cols-10.gap-10
-    div(:class="($slots.toc) ? 'col-span-8' : 'col-span-10'")
+    div(:class="($slots.toc) ? 'col-span-10 md:col-span-8' : 'col-span-10'")
       TntBreadcrumbs(:collection="collection")/
 
       slot(name="header")
+
+      //- TODO: On sm and lower, we also want Toc here!
 
       slot
 
       TntPrevNext(:collection="collection")/
 
-    .col-span-2(v-if="$slots.toc")
+    .col-span-2(v-if="$slots.toc" class="hidden md:flex")
       slot(name="toc")
 </template>
--- packages/content/layouts/doc.vue
+++ packages/content/layouts/doc.vue
@@ -7,18 +7,20 @@ const { collection = 'pages' }: { collection?: keyof PageCollections | undefined
 <template lang="pug">
 NuxtLayout(name="base")
   .grid.grid-cols-10.gap-10
-    .col-span-2(v-if="$slots.nav")
+    .col-span-2(v-if="$slots.nav" class="hidden md:flex")
       slot(name="nav")
 
-    div(:class="($slots.nav && $slots.toc) ? 'col-span-6' : ($slots.nav || $slots.toc) ? 'col-span-8' : 'col-span-10'")
+    div(:class="($slots.nav && $slots.toc) ? 'col-span-10 md:col-span-6' : ($slots.nav || $slots.toc) ? 'col-span-10 md:col-span-8' : 'col-span-10'")
       TntBreadcrumbs(:collection="collection")/
 
       slot(name="header")
+
+      //- TODO: On sm and lower, we also want Toc here!
 
       slot
 
       TntPrevNext(:collection="collection")/
 
-    .col-span-2(v-if="$slots.toc")
+    .col-span-2(v-if="$slots.toc" class="hidden md:flex")
       slot(name="toc")
 </template>
