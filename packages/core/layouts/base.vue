1d9df64ce33ed7472fd092927fc17281e1eecc2c
Thom Bruce <thom@thombruce.com>
2025-05-04 16:51:20 +0100

    remove flex classes from main container

--- packages/core/layouts/base.vue
+++ packages/core/layouts/base.vue
@@ -3,7 +3,13 @@ const runtimeConfig = useRuntimeConfig()
 </script>
 
 <template lang="pug">
-div(class="flex flex-col h-screen justify-between")
+div
+  //- TODO: Removed element with classes like:
+  //- div(class="flex flex-col h-screen justify-between")
+  //- This resulted in a bug on mobile whereby on navigation,
+  //- page would not scroll. Hard refresh corrected the problem,
+  //- but this just isn't how Nuxt navigation works.
+
   //- TODO: Maybe prefer to gracefully enhance content components
   //-       based on runtimeConfig, rather than duplicating or
   //-       overwriting them like this.
