510601d5f18476037fc6c10e32012cfd3fa434fa
Thom Bruce <thom@thombruce.com>
2025-05-06 23:12:15 +0100

    restore sticky-footer

--- packages/core/layouts/base.vue
+++ packages/core/layouts/base.vue
@@ -3,13 +3,7 @@ const runtimeConfig = useRuntimeConfig()
 </script>
 
 <template lang="pug">
-div
-  //- TODO: Removed element with classes like:
-  //- div(class="flex flex-col h-screen justify-between")
-  //- This resulted in a bug on mobile whereby on navigation,
-  //- page would not scroll. Hard refresh corrected the problem,
-  //- but this just isn't how Nuxt navigation works.
-
+div(class="flex flex-col h-screen justify-between")
   //- TODO: Maybe prefer to gracefully enhance content components
   //-       based on runtimeConfig, rather than duplicating or
   //-       overwriting them like this.
