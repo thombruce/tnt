d7ffd3247eee5c304080f9dbd324b470ef96d1a7
Thom Bruce <thom@thombruce.com>
2025-05-06 23:11:21 +0100

    add web module

---
+++ packages/web/nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+})
+
+// TODO:
+// 1. Cookie notice and consent
+// 2. Analytics tokens (active only after consent)
