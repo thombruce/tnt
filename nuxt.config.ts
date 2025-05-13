61964ef2ae2c84b3537e2a6e4837bef3149020a2
Thom Bruce <thom@thombruce.com>
2025-05-13 23:21:10 +0100

    migrate tnt.config to nuxt.config for simplicity and hot module reloading

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import { siteConfig, routeRules } from './tnt.config'
-
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  site: {
+    name: 'My Awesome Site',
+    description: 'Where I post all my awesome stuff!',
+    url: 'https://example.com/',
+    copyright: 'Copyright © All rights reserved',
+    nav: true,
+    backgroundPattern: false,
+  },
+  routeRules: {
+    /* Custom route rules */
+  },
+  runtimeConfig: {
+    public: {
+      collections: [
+        /* Custom content collections */
+      ],
+    },
+  },
+  ui: {
+    theme: {
+      colors: [
+        'primary',
+        'secondary',
+        'success',
+        'info',
+        'warning',
+        'error',
+        'neutral',
+      ],
+    },
+  },
   extends: [
     './packages/core',
     './packages/fancy',
@@ -14,15 +43,4 @@ export default defineNuxtConfig({
   modules: [
     /* Custom Modules */
   ],
-  site: {
-    name: siteConfig.name || 'TNT',
-    description: siteConfig.description || 'Welcome to TNT!',
-    url: siteConfig.url || 'https://example.com/',
-    copyright: siteConfig.copyright || 'Copyright © All rights reserved',
-    nav: siteConfig.nav || false,
-    backgroundPattern: siteConfig.backgroundPattern || false,
-  },
-  routeRules: {
-    ...routeRules,
-  },
 })
