f04bddfa685f460675dd27f4f4de00aa3c2cf5b4
Thom Bruce <thom@thombruce.com>
2025-05-13 18:00:06 +0100

    add custom ProsePre component

---
+++ packages/content/components/content/ProsePre.vue
@@ -0,0 +1,23 @@
+<!--
+  Overrides the default ProsePre provided by Nuxt Content.
+  This enables us to more easily set light and dark color
+  themes, which are otherwise difficult to negotiate between
+  conflicting rules of Tailwind Typography and Shiki language
+  highlighting.
+-->
+
+<script setup lang="ts">
+const { language, filename } = defineProps<{
+  language?: string
+  filename?: string
+}>()
+</script>
+
+<template lang="pug">
+pre(class="relative bg-neutral-100 dark:bg-neutral-950")
+  slot/
+  div(
+    v-if="language || filename"
+    class="absolute top-0 right-0 px-4 py-2 text-neutral-500"
+  ) {{ language }}{{ language && filename ? ': ' : '' }}{{ filename }}
+</template>
