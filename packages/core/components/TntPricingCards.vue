e07d12a4a5882a57ffa8d212795ecf2773cd1502
Thom Bruce <thom@thombruce.com>
2025-05-31 19:43:37 +0100

    make pricing cards responsive

--- packages/core/components/TntPricingCards.vue
+++ packages/core/components/TntPricingCards.vue
@@ -9,6 +9,7 @@ const {
 </script>
 
 <template lang="pug">
-div(class="flex gap-2")
-  TntPricingCard(v-for="card in cards" v-bind="card" class="flex-grow")
+div(class="flex flex-wrap justify-center gap-4")
+  //- NOTE: basis-[fraction] values are set a level higher than desired due to issues with margin/padding/gap.
+  TntPricingCard(v-for="card in cards" v-bind="card" class="flex-grow basis-full md:basis-1/3 lg:basis-1/4 xl:basis-1/5")
 </template>
