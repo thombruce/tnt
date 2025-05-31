72a4d886d6eb3bff35de48c5ca40286e29121803
Thom Bruce <thom@thombruce.com>
2025-05-31 18:12:52 +0100

    add pricing cards

---
+++ packages/core/components/TntPricingCards.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// Plural
+
+const {
+  cards
+} = defineProps<{
+  cards: object[]
+}>()
+</script>
+
+<template lang="pug">
+div(class="flex gap-2")
+  TntPricingCard(v-for="card in cards" v-bind="card" class="flex-grow")
+</template>
