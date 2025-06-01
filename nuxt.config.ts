68bae7d8b6b0f484dd51fe8bc65048e8eab9d7e3
Thom Bruce <thom@thombruce.com>
2025-06-01 22:57:54 +0100

    remove hacky vite and nitro configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  nitro: {
-    static: true
-  },
+  // nitro: {
+  //   static: true
+  // },
   site: {
     name: 'My Awesome Site',
     description: 'Where I post all my awesome stuff!',
