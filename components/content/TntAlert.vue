3a06a4b497fb67470fcfa7a291aca72f50ef2545
Thom Bruce <thom@thombruce.com>
2025-06-11 18:12:42 +0100

    more options on TntAlert

--- components/content/TntAlert.vue
+++ components/content/TntAlert.vue
@@ -1,42 +1,52 @@
 <script setup lang="ts">
 // TODO: Explicitly list all props, even those that will be handled by UAlert.
 //       This will ensure that they show up in Studio.
-const { success, info, warning, error, large, title = undefined, description = undefined } = defineProps<{
-  success?: boolean
-  info?: boolean
-  warning?: boolean
-  error?: boolean
-  large?: boolean
-
+const {
+  title = undefined,
+  description = undefined,
+  template = undefined,
+  color = undefined,
+  icon = undefined,
+  variant = "subtle",
+  large
+} = defineProps<{
   title?: string
   description?: string
+  template?: "success" | "info" | "warning" | "error"
+
+  color?: "primary" | "secondary" | "success" | "info" | "warning" | "error" | "neutral"
+  icon?: string
+  variant?: "subtle" | "solid" | "outline" | "soft"
+
+  large?: boolean
 }>()
 
-let color: "primary" | "secondary" | "success" | "info" | "warning" | "error" | "neutral" | undefined, icon: string
+const computedColor = computed(() => {
+  return template || color
+})
 
-if (success) {
-  color = "success"
-  icon = "lucide:check" // "fluent-color:checkmark-circle-16"
-} else if (info) {
-  color = "info"
-  icon = "lucide:info" // "fluent-color:question-circle-16"
-} else if (warning) {
-  color = "warning"
-  icon = "lucide:triangle-alert" // "fluent-color:error-circle-16"
-} else if (error) {
-  color = "error"
-  icon = "lucide:ban" // "fluent-color:dismiss-circle-16"
-}
+const computedIcon = computed(() => {
+  if (icon) return icon
+  else if (template === "success") return "lucide:check" // "fluent-color:checkmark-circle-16"
+  else if (template === "info") return "lucide:info" // "fluent-color:question-circle-16"
+  else if (template === "warning") return "lucide:triangle-alert" // "fluent-color:error-circle-16"
+  else if (template === "error") return "lucide:ban" // "fluent-color:dismiss-circle-16"
+  else return undefined
+})
 
-const ui = large ? { title: 'text-lg md:text-xl', icon: 'text-2xl md:text-3xl' } : { icon: 'text-xl' }
+const computedUi = computed(() => {
+  return large ? { title: 'text-lg md:text-xl', icon: 'text-2xl md:text-3xl' } : { icon: 'text-xl' }
+})
 </script>
 
 <template lang="pug">
 UAlert(
-  :color="color"
-  :icon="icon"
-  :ui="ui"
-  variant="subtle"
+  :title="title"
+  :description="description"
+  :color="computedColor"
+  :icon="computedIcon"
+  :ui="computedUi"
+  :variant="variant"
   v-bind="$props"
 )
 </template>
