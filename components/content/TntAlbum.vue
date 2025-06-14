18460518585316c2da59b2071b4a599334517aff
Thom Bruce <thom@thombruce.com>
2025-06-14 02:28:48 +0100

    add reusable album component

---
+++ components/content/TntAlbum.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+div(class="flex flex-wrap justify-center gap-4")
+  slot/
+</template>
--- components/content/TntPricingCards.vue
+++ components/content/TntPricingCards.vue
@@ -17,7 +17,7 @@ const {
 </script>
 
 <template lang="pug">
-div(class="flex flex-wrap justify-center gap-4")
+TntAlbum
   //- NOTE: basis-[fraction] values are set a level higher than desired due to issues with margin/padding/gap.
   TntPricingCard(v-for="card in cards" v-bind="card" class="flex-grow basis-full md:basis-1/3 lg:basis-1/4 xl:basis-1/5")
 </template>
