b1b4d61a066708910469059025c010d1d4aba10c
Thom Bruce <thom@thombruce.com>
2025-06-15 19:07:40 +0100

    upgrade to nuxt 4 via future tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   nitro: {
