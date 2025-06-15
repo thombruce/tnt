4fca7587cba00831f7df3157b5f341b85c8ed2eb
Thom Bruce <thom@thombruce.com>
2025-06-15 16:44:56 +0100

    reduce to single-layout setup

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,34 @@
 <script lang="ts" setup>
-import type { PageCollections } from '@nuxt/content';
+import type { PageCollections } from '@nuxt/content'
+
+const { theme }: { theme?: 'solid' | 'ghost' } = useAttrs()
 
 const { collection = 'pages' }: { collection?: keyof PageCollections | undefined } = useAttrs()
 </script>
 
 <template lang="pug">
-NuxtLayout(name="base")
-  div(class="grid grid-cols-10 gap-10")
-    div(class="col-span-10")
-      TntBreadcrumbs(:collection="collection")/
+div(class="flex flex-col h-screen justify-between")
+  TntHeader(:variant="theme")/
 
-      slot(name="header")
+  UContainer(class="mb-auto")
+    div(class="grid grid-cols-10 gap-10")
+      div(v-if="$slots.nav" class="col-span-2 hidden md:flex")
+        slot(name="nav")
 
-      slot
+      div(:class="($slots.nav && $slots.toc) ? 'col-span-10 md:col-span-6' : ($slots.nav || $slots.toc) ? 'col-span-10 md:col-span-8' : 'col-span-10'")
+        TntBreadcrumbs(:collection="collection")/
 
-      TntPrevNext(:collection="collection")/
+        //- NOTE: header slot removed in order to favour in-document title
+        //- slot(name="header")
+
+        //- TODO: On sm and lower, we also want Toc here!
+
+        slot
+
+        TntPrevNext(:collection="collection")/
+
+      div(v-if="$slots.toc" class="col-span-2 hidden md:flex")
+        slot(name="toc")
+
+  TntFooter(:variant="theme")/
 </template>
--- layouts/missing.vue
+++
@@ -1,7 +0,0 @@
-<template lang="pug">
-NuxtLayout(name="base")
-  div(class="grid grid-cols-10 gap-10")
-    div(class="col-span-10")
-      h1(class="text-4xl font-extrabold") 404
-      p(class="text-2xl") Page not found
-</template>
