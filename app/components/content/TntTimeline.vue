a9fd3414ea963e87bf505dd2d1d34292dde48fad
Thom Bruce <thom@thombruce.com>
2025-06-17 23:08:36 +0100

    use Nuxt UI pre-release and add TntTimeline component

---
+++ app/components/content/TntTimeline.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { TimelineItem } from '@nuxt/ui'
+
+const {
+  items,
+} = defineProps<{
+  items: TimelineItem[]
+}>()
+</script>
+
+<template lang="pug">
+UTimeline(:default-value="items.length - 1" :items="items" class="w-96 m-auto")/
+</template>
