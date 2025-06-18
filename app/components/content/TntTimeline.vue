81b78cd361232f34fe0afb588c28fb3c7820c77a
Thom Bruce <thom@thombruce.com>
2025-06-18 04:38:32 +0100

    add additional props to Timeline component

--- app/components/content/TntTimeline.vue
+++ app/components/content/TntTimeline.vue
@@ -1,13 +1,35 @@
 <script setup lang="ts">
-import type { TimelineItem } from '@nuxt/ui'
+import type { AppConfig } from '@nuxt/schema'
+import theme from '#build/ui/timeline'
+import type { ComponentConfig, TimelineItem } from '@nuxt/ui'
+
+type Timeline = ComponentConfig<typeof theme, AppConfig, 'timeline'>
 
 const {
   items,
+  color,
+  size,
+  // orientation,
+  reverse,
 } = defineProps<{
   items: TimelineItem[]
+  color: Timeline['variants']['color']
+  size: Timeline['variants']['size']
+  // TODO: orientation cannot be used until width stylings are modified
+  //       ideally also allow for x-overflow scroll behavior
+  // orientation?: Timeline['variants']['orientation']
+  reverse?: boolean
 }>()
 </script>
 
 <template lang="pug">
-UTimeline(:default-value="items.length - 1" :items="items" class="max-w-128 m-auto")/
+UTimeline(
+  :default-value="items.length - 1"
+  v-bind="$props"
+  :items="items"
+  :color="color"
+  :size="size"
+  :reverse="reverse"
+  class="max-w-128 m-auto"
+)/
 </template>
