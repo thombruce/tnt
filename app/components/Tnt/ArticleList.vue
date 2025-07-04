99ce59db39cbfb72060643a63c35344c6baedcd2
Thom Bruce <thom@thombruce.com>
2025-07-04 13:26:24 +0100

    add grid component

--- app/components/Tnt/ArticleList.vue
+++ app/components/Tnt/ArticleList.vue
@@ -18,31 +18,23 @@ const items = await queryCollection(collection as keyof PageCollections)
 </script>
 
 <template lang="pug">
-div(class="my-4 space-y-4")
-  ULink(v-if="items?.length" v-for="item in items" :to="item.path" class="block")
-    UCard
-      div(class="grid grid-cols-10 gap-10")
-        div(:class="item.image ? 'col-span-7' : 'col-span-10'")
-          div(class="space-x-3")
-            //- TODO: Wouldn't it be cool to be able to define custom metadata?
-            //- Perhaps we can achieve this with scoped slots passing the retrieved
-            //- page data back to the component invocation.
-            UBadge(v-if="'categories' in item && item.categories" v-for="category in item.categories" color="neutral" variant="outline") {{ category }}
-            strong(v-if="'date' in item && item.date") {{ new Date(item.date).toLocaleDateString() }}
-          div(class="space-y-2")
-            h2(class="text-2xl font-extrabold") {{ item.title }}
-            p {{ item.description }}
-          div
-            div(
-              v-if="'authors' in item && item.authors"
-              v-for="author in item.authors"
-              class="px-2.5 py-1.5 text-sm space-x-1.5 inline-block"
-            )
-              UAvatar(:src="author.avatar?.src" size="2xs")
-              span {{ author.name }}
-        div(v-if="item.image" class="col-span-3")
-          div(class="block aspect-square")
-            NuxtImg(:src="item.image" fit="cover" width="600" height="600" class="w-full h-full object-cover")
+div
+  TntGrid(v-if="items?.length" :items="items")
+    template(#item="{ path, categories, date, title, description, authors }")
+      ULink(:to="path")
+        UCard
+          UBadge(v-if="categories" v-for="category in categories" color="neutral" variant="outline") {{ category }}
+          strong(v-if="date") {{ new Date(date).toLocaleDateString() }}
+          h2(class="text-2xl font-extrabold") {{ title }}
+          p {{ description }}
+          div(
+            v-if="authors"
+            v-for="author in authors"
+            class="px-2.5 py-1.5 text-sm space-x-1.5 inline-block"
+          )
+            UAvatar(:src="author.avatar?.src" size="2xs")
+            span {{ author.name }}
+
   div(v-else)
     p No items to show.
 </template>
---
+++ app/components/Tnt/Grid.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const { items, cols = 4 } = defineProps<{
+  items?: any[]
+  cols?: 1 | 2 | 3 | 4
+  // masonry?: boolean
+}>()
+
+// TODO: This is an imperfect way of handling the number of columns.
+//       Perhaps we would prefer to invoke Tailwind Variants UI manipulation?
+
+const klass = computed(() => {
+  switch (cols) {
+    case 1:
+      return 'grid-cols-1'
+    case 2:
+      return 'grid-cols-1 sm:grid-cols-2'
+    case 3:
+      return 'grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3'
+    case 4:
+    default:
+      return 'grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'
+  }
+})
+</script>
+
+<template lang="pug">
+div(class="grid gap-4" :class="klass")
+  div(v-for="item in items" class="")
+    slot(name="item" v-bind="item")/
+</template>
