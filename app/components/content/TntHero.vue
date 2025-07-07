181e743a4aaa2c0fbb734fcd26eb5391c1e9916a
Thom Bruce <thom@thombruce.com>
2025-07-07 01:17:49 +0100

    add basic hero component

---
+++ app/components/content/TntHero.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { ButtonProps } from "@nuxt/ui"
+
+const { title, description, headline, links, orientation = 'vertical' } = defineProps<{
+  title?: string
+  description?: string
+  headline?: string
+  links?: ButtonProps[]
+  orientation?: 'vertical' | 'horizontal'
+  // reverse?: boolean
+}>()
+</script>
+
+<template lang="pug">
+section(
+  class="not-prose w-full max-w-(--ui-container) mx-auto px-4 sm:px-6 lg:px-8 flex flex-col py-24 sm:py-32 gap-16 sm:gap-y-24"
+  :class="orientation === 'horizontal' ? 'lg:grid lg:py-40 lg:grid-cols-2 lg:items-center' : ''"
+)
+  div(v-if="title || description || headline || links")
+    span(class="mb-4 font-semibold text-primary flex items-center gap-1.5") {{ headline }}
+    h1(class="text-5xl sm:text-7xl text-pretty tracking-tight font-bold text-highlighted") {{ title }}
+    p(class="text-lg sm:text-xl/8 text-muted text-pretty mt-6") {{ description }}
+  div(v-if="$slots.default")
+    slot/
+</template>
