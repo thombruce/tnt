88c3da97aede1d02769045fc3b8c0485bcff9ea1
Thom Bruce <thom@thombruce.com>
2025-07-31 03:57:02 +0100

    move collapsible toc to own component

---
+++ app/components/Tnt/TocCollapsible.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { Toc } from '@nuxt/content';
+
+const {
+  toc
+} = defineProps<{
+  toc: Toc
+}>()
+
+const tocOpen = ref(false)
+
+// TODO: Still broken; please investigate
+</script>
+
+<template lang="pug">
+UCollapsible(v-model:open="tocOpen" :unmount-on-hide="false" class="flex flex-col gap-2 md:hidden")
+  UButton(
+    label="On this page"
+    color="neutral"
+    variant="subtle"
+    trailing-icon="lucide:chevron-down"
+    block
+    @click="tocOpen = !tocOpen"
+  )/
+  template(#content)
+    TntToc(:toc="toc")
+</template>
--- app/layouts/docs.vue
+++ app/layouts/docs.vue
@@ -8,8 +8,6 @@ const navItems = await tntNav(true, collection)
 
 const showNav = computed(() => page?.nav !== false && navItems)
 const showToC = computed(() => page?.toc !== false && page?.body.toc?.links.length)
-
-const tocOpen = ref(false)
 </script>
 
 <template lang="pug">
@@ -27,18 +25,7 @@ NuxtLayout(:theme="theme" :collection="collection")
 
     div(:class="(showNav && showToC) ? 'col-span-10 md:col-span-6' : (showNav || showToC) ? 'col-span-10 md:col-span-8' : 'col-span-10'")
 
-      //- TODO: Broken; please investigate
-      UCollapsible(v-if="showToC" v-model:open="tocOpen" :unmount-on-hide="false" class="flex flex-col gap-2 md:hidden")
-        UButton(
-          label="On this page"
-          color="neutral"
-          variant="subtle"
-          trailing-icon="lucide:chevron-down"
-          block
-          @click="tocOpen = !tocOpen"
-        )/
-        template(#content)
-          TntToc(:toc="page?.body.toc")
+      TntTocCollapsible(v-if="showToC" :toc="page?.body.toc")/
 
       slot
 
