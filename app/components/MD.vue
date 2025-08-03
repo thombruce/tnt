845e47451ede3c3624cd9a07bee589525459e4ab
Thom Bruce <thom@thombruce.com>
2025-08-03 20:17:49 +0100

    add MDC-inspired MD component and prepare content config for data types denoted by _

---
+++ app/components/MD.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import rehypeSanitize from 'rehype-sanitize'
+import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import {unified} from 'unified'
+
+const { value } = defineProps<{
+  value: string
+}>()
+
+const html = await unified()
+  .use(remarkParse)
+  .use(remarkRehype)
+  .use(rehypeSanitize)
+  .use(rehypeStringify)
+  .process(value)
+</script>
+
+<template lang="pug">
+div(v-html="html")
+</template>
